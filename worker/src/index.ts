12f648ee87332fd590552994da4b7fb0740ad9e5
Lukas Hahn <lukas1h07@gmail.com>
2025-04-01 09:54:29 -0700

    Change file path and make repo not hardcoded.

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -3,15 +3,53 @@ import { env, getRuntimeKey } from "hono/adapter";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.get("/", async (c) => {
+  const path = "/";
+  const apiUrl = `https://api.github.com/repos/${env(c).REPO}/contents/${path}`;
+  console.log(`Bearer ${env(c).GITHUB_TOKEN}`);
+  const response = await fetch(apiUrl, {
+    headers: {
+      "User-Agent": "lukashahn.art",
+      Authorization: `Bearer ${env(c).GITHUB_TOKEN}`,
+    },
+    method: "GET",
+  });
+  if (!response.ok) {
+    return c.text(await response.text(), 500);
+  }
+
+  const contents = await response.json();
+  if (Array.isArray(contents)) {
+    const listItems = contents
+      .map((item) => {
+        if (item.type === "file") {
+          return `<a href="${`https://github.com/${env(c).REPO}/blob/main/${
+            item.path
+          }?raw=true`}">${item.name}</a>`;
+        } else if (item.type === "dir") {
+          return `<a href="${`/${item.path}/`}">${item.name}/</a>`;
+        }
+      })
+      .join("<br>\n");
+
+    let html = `
+      <p>${path}</p>
+      <ul>${listItems}</ul>
+      `;
+
+    return c.html(listItems);
+  } else {
+    return c.text("Invalid directory contents.", 500);
+  }
 });
 
-app.get("/files/:path{.+}", async (c) => {
+app.get("/:path{.+}", async (c) => {
   const path = c.req.param("path");
 
   if (path === "" || path.endsWith("/")) {
-    const apiUrl = `https://api.github.com/repos/Lukas1h/filesharing/contents/${path}`;
+    const apiUrl = `https://api.github.com/repos/${
+      env(c).REPO
+    }/contents/${path}`;
     console.log(`Bearer ${env(c).GITHUB_TOKEN}`);
     const response = await fetch(apiUrl, {
       headers: {
@@ -29,11 +67,11 @@ app.get("/files/:path{.+}", async (c) => {
       const listItems = contents
         .map((item) => {
           if (item.type === "file") {
-            return `<a href="${`https://github.com/Lukas1h/filesharing/blob/main/${item.path}?raw=true`}">${
-              item.name
-            }</a>`;
+            return `<a href="${`https://github.com/${env(c).REPO}/blob/main/${
+              item.path
+            }?raw=true`}">${item.name}</a>`;
           } else if (item.type === "dir") {
-            return `<a href="${`/files/${item.path}/`}">${item.name}/</a>`;
+            return `<a href="${`/${item.path}/`}">${item.name}/</a>`;
           }
         })
         .join("<br>\n");
@@ -50,7 +88,7 @@ app.get("/files/:path{.+}", async (c) => {
   }
 
   return c.redirect(
-    `https://github.com/Lukas1h/filesharing/blob/main/${path}?raw=true`
+    `https://github.com/${env(c).REPO}/blob/main/${path}?raw=true`
   );
 });
 
